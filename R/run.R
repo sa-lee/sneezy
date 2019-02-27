888191fd2c5183d57cdded040fd98662c50d4e9c
Stuart Lee <lee.s@wehi.edu.au>
2019-02-27 15:13:08 +1100

    giftastic

--- R/run.R
+++ R/run.R
@@ -30,12 +30,22 @@ basic_tsne <- function(data, perplexity ) {
 view_tour <- function(data, tsne_coords, .subset = NULL) {
   
   nn_graph <- get_neighbourhood_graph(tsne_coords, .subset)
+  dir <- tempdir()
+  png_path <- file.path(dir, "frame%03d.png")
   
-  tourr::animate(data, 
-                 tour_path = tourr::grand_tour(),
-                 tourr::display_xy(axes = "bottomleft", 
-                                   edges = nn_graph)
+  tourr::render(data,
+                tour_path = tourr::grand_tour(),
+                 display = tourr::display_xy(axes = "bottomleft", 
+                                             edges = nn_graph),
+                dev = "png",
+                png_path,
+                frames = 100
   )
+  png_files <- sprintf(png_path, 1:100)
+  gif_file <- tempfile(fileext = ".gif")
+  gifski::gifski(png_files, gif_file, delay = 0.25, progress = FALSE)
+  on.exit(unlink(png_files))
+  gganimate::gif_file(gif_file)
 }
 
 #' Simplified grand tour history
