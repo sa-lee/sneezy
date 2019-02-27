6be7cc67b7fd90d0695684e6b818512049b19867
Stuart Lee <lee.s@wehi.edu.au>
2019-02-27 14:37:49 +1100

    subset options

--- R/neighbours.R
+++ R/neighbours.R
@@ -1,26 +1,41 @@
 #' Generate nearest neighbours graph in t-SNE space
 #' 
 #' @param tnse_coords the list obtained from running `Rtsne::Rtsne()`
+#' @param .subset a vector of indices to find nearest neighbours, by default
+#' all nearest neighbours will be used. 
+#' 
+#' @return an integer matrix with columns from and to
+#' 
+#' @importFrom BiocNeighbors findKNN
 #' 
 #' @export
-get_neighbourhood_graph <- function(tsne_coords) {
+get_neighbourhood_graph <- function(tsne_coords, .subset = NULL) {
   # find nearest neighbours based on perplexity
   perpelexity <- tsne_coords$perplexity
   coords <- tsne_coords$Y
   
-  n <- nrow(coords)
   K <- 3 * perpelexity
   
-  nn <- BiocNeighbors::findKNN(coords, k = K)
-  
+  nn <- BiocNeighbors::findKNN(coords, 
+                               k = K, 
+                               subset = .subset, 
+                               get.index = TRUE,
+                               get.distance = FALSE)
   # flatten as an adjancey matrix
   edges <- nn$index
+  
+  # set up dimensions
+  if (!is.null(.subset)) {
+    n <- length(.subset)
+  } else {
+    n <- nrow(coords)
+    .subset <- seq_len(n)
+  }
+  # reshape to include from edges
   dim(edges) <- c(n * K, 1)
-  # reshape and add in identity edges
-  edges <- cbind(rep(seq_len(n), each = K), 
-                 edges)
-  edges <- rbind(cbind(seq_len(n), seq_len(n)),
-                 edges)
+  edges <- cbind(rep(.subset, each = K), edges)
   colnames(edges) <- c("from", "to")
   edges 
 }
+
+
