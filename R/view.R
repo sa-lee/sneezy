00e772dbc78c3cd56bbb4422798ed70f6e14a10c
Stuart Lee <lee.s@wehi.edu.au>
2019-02-27 14:37:23 +1100

    experimental tour plots with vegawidget + shiny

---
+++ R/view.R
@@ -0,0 +1,83 @@
+spec_tour <- function(dim_chart) {
+  vegawidget::as_vegaspec(
+    list(
+      `$schema` = vegawidget::vega_schema(),
+      width = dim_chart,
+      height = dim_chart,
+      data = list(name = "projection"),
+      mark = list(type = "circle"),
+      encoding = list(
+        x = list(
+          field = "V1",
+          type = "quantitative",
+          axis = list(title = NULL)
+        ),
+        y = list(
+          field = "V2",
+          type = "quantitative",
+          axis = list(title = NULL),
+          scale = list(
+            domain = c()      
+          )
+        )
+      )
+    )
+  )
+}
+
+ui_tour <- function(history) {
+  d <- dim(history)
+  
+  ui_slider <-
+    shiny::sliderInput(
+      "slider",
+      label = "basis",
+      min = 1L,
+      max = d[3],
+      step = 1L,
+      value = 1L,
+      sep = "",
+      animate = shiny::animationOptions(interval = 500, loop = TRUE)
+    )
+  
+  shiny::fluidPage(
+    shiny::titlePanel("Tourring"),
+    shiny::sidebarLayout(
+      shiny::sidebarPanel(
+        ui_slider
+      ),
+      shiny::mainPanel(
+        vegawidget::vegawidgetOutput("chart")
+      )
+    )
+  )
+}
+
+shiny_tour <- function(data, tsne_coords, .subset) {
+  history <- basic_tour_path(data)
+  nn_graph <- get_neighbourhood_graph(tsne_coords, .subset)
+  nn_index <- c(nn_graph[1,1], unique(nn_graph[,2]))
+  spec <- spec_tour(400)
+  server <- function(input, output) {
+    # reactives
+    
+    current_projection <- shiny::reactive({
+      source <- as.data.frame(data %*% history[,,input$slider])
+      source$group <- NA
+      source
+      # nn_source <- source[nn_graph, ]
+      # nn_source$group <- rep(seq_len(nrow(nn_graph)), 2)
+      # nn_exclude <- source[-nn_index, ]
+      # nn_exclude$group <- NA
+      # rbind(nn_exclude, nn_source)
+    })
+    
+    vegawidget::vw_shiny_set_data("chart", "projection", current_projection())
+    
+    output$chart <- vegawidget::renderVegawidget(spec)
+  }
+  
+  ui <- ui_tour(history)
+  shiny::shinyApp(ui, server)
+}
+
