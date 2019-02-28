154dd2fdc7d8ef943b7fbf381ac02b69791e5e9b
Stuart Lee <lee.s@wehi.edu.au>
2019-02-28 13:39:58 +1100

    simple triangulation

--- R/run.R
+++ R/run.R
@@ -5,16 +5,19 @@
 #' normalising the input data or running PCA beforehand.
 #' 
 #' @param data a data.frame or matrix to compute t-SNE on
-#' @param perplexity the perpelxity paramater to t-SNE algorithm (see `Rtsne::Rtsne()`)
+#' @param perplexity the perpelxity paramater to t-SNE algorithm (see [Rtsne::Rtsne()])
+#' @param alpha the exaggeration factor for the intial part of the optimsation 
 #' 
-#' 
+#' @seealso 
+#' [Rtsne::Rtsne()]
 #' @export
-basic_tsne <- function(data, perplexity ) {
+basic_tsne <- function(data, perplexity, alpha = nrow(data) / 10) {
   Rtsne::Rtsne(data,
                perplexity = perplexity,
+               exaggeration_factor = alpha,
                theta = 0,
                pca = FALSE,
-               normalize_input = FALSE)
+               normalize = TRUE)
 }
 
 #' Animate grand tour with t-SNE neighbourhood graph
@@ -24,15 +27,42 @@ basic_tsne <- function(data, perplexity ) {
 #' @param tsne_coords a list from running `Rtsne::Rtsne()`
 #' @param .subset a vector of indices to find nearest neighbours, by default
 #' all nearest neighbours will be used.
-#' 
+#' @param ... other control options passed to [tourr::display_xy()]
 #' 
 #' @export
-view_tour <- function(data, tsne_coords, .subset = NULL) {
-  
+sneezy_neighbours <- function(data, tsne_coords, .subset = NULL, ...) {
   nn_graph <- get_neighbourhood_graph(tsne_coords, .subset)
-  gif_tour(data, nn_graph)
+  gif_tour(data, nn_graph, ...)
   
 }
+
+#' @export
+sneezy_triangles <- function(data,  tsne_coords, ...) {
+  edges <- get_triangles(tsne_coords)
+  gif_tour(data, edges, ...)
+}
+
+
+
+#' @export
+sneezy_centroids <- function(data, tsne_coords) {
+  centroids <- get_centroids_from_nn(data, tsne_coords)
+  col <- c(rep("black", nrow(data)), rep("red", nrow(centroids)))
+  data <- rbind(data, centroids)
+  gif_tour(data, edges = NULL, col = col)
+  
+}
+#' Simplified grand tour history
+#' 
+#' @export
+basic_tour_path <- function(data) {
+  projections <- tourr::save_history(data, rescale = FALSE)
+  attr(projections, "data") <- NULL
+  class(projections) <- NULL
+  projections
+}
+
+
 
 gif_tour <- function(data, edges, ...) {
   dir <- tempdir()
@@ -52,22 +82,4 @@ gif_tour <- function(data, edges, ...) {
   gifski::gifski(png_files, gif_file, delay = 0.25, progress = FALSE)
   on.exit(unlink(png_files))
   gganimate::gif_file(gif_file)
-}
-
-#' @export
-view_tour_nn_centroids <- function(data, tsne_coords) {
-  centroids <- get_centroids_from_nn(data, tsne_coords)
-  col <- c(rep("black", nrow(data)), rep("red", nrow(centroids)))
-  data <- rbind(data, centroids)
-  gif_tour(data, edges = NULL, col = col)
-  
-}
-#' Simplified grand tour history
-#' 
-#' @export
-basic_tour_path <- function(data) {
-  projections <- tourr::save_history(data, rescale = FALSE)
-  attr(projections, "data") <- NULL
-  class(projections) <- NULL
-  projections
 }
---
+++ R/triangles.R
@@ -0,0 +1,12 @@
+#' Compute Delany triangulation in t-SNE space
+#' 
+#' @param coords output from [Rtsne::Rtsne()]
+#' 
+get_triangles <- function(coords) {
+  tri <- geometry::delaunayn(coords$Y)
+  rbind(
+    tri[, c(1,2)],
+    tri[, c(2,3)],
+    tri[, c(3,1)]
+  )
+}
