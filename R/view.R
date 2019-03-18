c6a659f1e8ee2ea8979b2b85125132270be015da
Stuart Lee <lee.s@wehi.edu.au>
2019-03-18 14:33:22 +1100

    shiny prototype with linked brushing

--- R/view.R
+++ R/view.R
@@ -1,23 +1,11 @@
-spec_tour <- function(data, half_range) {
+spec_tour <- function(half_range) {
   domain <- c(-half_range, half_range)
   
   tour_layer <- list(
-    data = list(name = "projections", values = data),
-    transform = list(
-      list(filter =list(selection = "path"))
-    ),
-    selection = list(
-      path = list(
-        type = "single",
-        fields = list("key"),
-        bind = list("key" = 
-                      list(input = "range", 
-                           min = min(data$key), 
-                           max = max(data$key), 
-                           step = 1)
-                    )
-        )
-    ),
+    # transform = list(
+    #   list(filter =list(selection = "path"))
+    # ),
+    #selection = list(),
     mark = list(type = "circle", clip = TRUE),
     encoding = list(
       x = list(field = "V1", type = "quantitative", 
@@ -26,18 +14,23 @@ spec_tour <- function(data, half_range) {
                            grid = FALSE, 
                            ticks = FALSE,
                            labels = FALSE)
-               ),
+      ),
       y = list(field = "V2", type = "quantitative",
                scale = list(domain = domain),
                axis = list(title = NULL, 
                            grid = FALSE, 
                            ticks = FALSE,
                            labels = FALSE)
-               ),
-      color = list(value = "black")
+      ),
+      color = list(
+        condition = list(
+          selection = "brush",
+          value = "black"
+        ),
+        value = "gray")
     )
   )
-
+  
   tour_layer
 }
 
@@ -48,53 +41,134 @@ spec_tour <- function(data, half_range) {
 # - toggle centroids on/off
 spec_projection <- function(coords) {
   half_range <- max(sqrt(rowSums(coords$Y^2)))
-  values <- data.frame(x = coords$Y[,1], y = coords$Y[,2])
   domain <- c(-half_range, half_range)
   nl_layer <- list(
-    data = list(name = "nl", values = values),
     mark = list(type = "circle", clip = TRUE),
+    selection = list(brush = list(type = "interval")),
     encoding = list(
-      x = list(field = "x", type = "quantitative", 
+      x = list(field = "tsne_x", type = "quantitative", 
                scale = list(domain = domain),
                axis = list(title = NULL, 
                            grid = FALSE, 
                            ticks = FALSE,
                            labels = FALSE)
       ),
-      y = list(field = "y", type = "quantitative",
+      y = list(field = "tsne_y", type = "quantitative",
                scale = list(domain = domain),
                axis = list(title = NULL, 
                            grid = FALSE, 
                            ticks = FALSE,
                            labels = FALSE)
       ),
-      color = list(value = "black")
+      color = list(
+        condition = list(
+          selection = "brush",
+          value = "black"
+        ),
+        value = "gray")
     )
   )
   nl_layer
 }
 
-setup_frames <- function(data) {
-  history <- basic_tour_path(data)
+
+
+
+
+
+sneezy <- function(data, max_bases, coords) {
+  if (!requireNamespace("shiny", quietly = TRUE)) {
+    stop("Please install shiny", call. = FALSE)
+  }
+  
+  ui <- sneezy_ui(max_bases)
+  server <- sneezy_server(data, max_bases, coords)
+  
+  shiny::shinyApp(ui, server)
+  
+}
+
+sneezy_ui <- function(max_bases) {
+  
+  tour_slider <- shiny::sliderInput(
+    "n_bases",
+    label = "Number of bases",
+    min = 1,
+    value = 1,
+    max = max_bases,
+    step = 1,
+    sep = "",
+    animate = shiny::animationOptions(interval = 200, loop = TRUE)
+  )
+  
+  
+  shiny::fluidPage(
+    shiny::sidebarLayout(
+      shiny::mainPanel(
+        vegawidget::vegawidgetOutput("chart")
+      ),
+      shiny::sidebarPanel(tour_slider)
+    )
+  )
+}
+
+sneezy_server <- function(data, max_bases, coords) {
+  history <- basic_tour_path(data, max_bases = max_bases)
+  # readust data
   tour_data <- tourr::rescale(data)
   tour_data <- scale(tour_data, center = TRUE, scale = FALSE)
   half_range  <- max(sqrt(rowSums(tour_data^2)))
-  projections <- apply(history, 2, function(.) `%*%`(tour_data, .) )
-  projections <- as.data.frame(projections)
-  projections$key <- rep(seq_len(100), each = nrow(tour_data))
-  projections$index <- rep(seq_len(nrow(tour_data)), 100)
-  list(data = projections, half_range = half_range)
-}
 
-sneezy <- function(data, coords, dim_chart) {
-  input <- setup_frames(data)
-  panel_tour <- spec_tour(input$data, input$half_range)
+  # static tsne_projection
+  tbl_tsne <- data.frame(tsne_x = coords$Y[,1], 
+                         tsne_y = coords$Y[,2])
+  
+  tbl_tour <- as.data.frame(tour_data %*% history[,,1])
+  tbl_init <- cbind(tbl_tour, tbl_tsne)
+  
+  # set up panels
+  panel_tour <- spec_tour(half_range)
   panel_tsne <- spec_projection(coords)
+  
+  
+  # final spec
   spec <- list(
     `$schema` = vegawidget::vega_schema(),
-    width = 2*dim_chart,
-    height = dim_chart,
+    data = list(name = "projections", values = tbl_init),
     hconcat = list(panel_tour, panel_tsne)
   )
-  vegawidget::as_vegaspec(spec)
+  spec <- vegawidget::as_vegaspec(spec)
+  function(input, output) {
+    rct_data <- shiny::reactive({
+      tbl_init[, c(1,2)] <-tour_data %*% history[,,input$n_bases]
+      tbl_init
+    })
+    
+    vegawidget::vw_shiny_set_data("chart", "projections", rct_data())
+    output$chart <- vegawidget::renderVegawidget(spec)
+  }
+  
 }
+
+# sneezy <- function(data, coords, dim_chart) {
+#   input <- setup_frames(data)
+#   
+#   
+#   panel_tour <- spec_tour(input$data, input$half_range)
+#   panel_tsne <- spec_projection(coords)
+# 
+# }
+
+
+
+# setup_frames <- function(data) {
+#   history <- basic_tour_path(data)
+#   tour_data <- tourr::rescale(data)
+#   tour_data <- scale(tour_data, center = TRUE, scale = FALSE)
+#   half_range  <- max(sqrt(rowSums(tour_data^2)))
+#   projections <- apply(history, 2, function(.) `%*%`(tour_data, .) )
+#   projections <- as.data.frame(projections)
+#   projections$key <- rep(seq_len(100), each = nrow(tour_data))
+#   projections$index <- rep(seq_len(nrow(tour_data)), 100)
+#   list(data = projections, half_range = half_range)
+# }
