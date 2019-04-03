af6146a6e237be52e215fd95954139e9a6bcff7f
Stuart Lee <lee.s@wehi.edu.au>
2019-04-03 15:46:03 +1100

    add in fast sphering via irlba

---
+++ R/utils.R
@@ -0,0 +1,16 @@
+#' Fast sphering of data
+#' 
+#' @param data a numerical matrix 
+#' @param k number of principal components to return
+#' @importFrom irlba prcomp_irlba
+#' 
+#' @description Like tourr::sphere_data but gives you control 
+#' over number of components returned.
+#' 
+fsphere_data <- function(data, k) {
+  if (!requireNamespace("irlba", quietly = TRUE)) {
+    stop("Fast sphering requires the irlba package to be installed.")
+  }
+  dr <- irlba::prcomp_irlba(data, k)
+  scale(stats::predict(dr))
+}
