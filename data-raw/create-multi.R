00bd2064853a753b1f8830c5d3af896f8cead502
Stuart Lee <lee.s@wehi.edu.au>
2019-08-21 11:38:22 +1000

    update multi data object

--- data-raw/create-multi.R
+++ data-raw/create-multi.R
@@ -1,5 +1,6 @@
 library(readr)
 library(dplyr)
+library(sneezy)
 
 # this is example highD data from 
 # http://ifs.tuwien.ac.at/dm/dataSets.html
@@ -11,5 +12,11 @@ multi <- read_tsv("http://ifs.tuwien.ac.at/dm/download/multiChallenge-matrix.txt
     key = rep(LETTERS[1:5], each = 200L)
   ) %>%
   select(key, index, X1:X10)
+
+# turn it into a SightSE object
+mutli <- as_sightse(multi, 
+                    traveller = tourr::grand_tour(), 
+                    X1:X10)
+
 
 usethis::use_data(multi, overwrite = TRUE)
