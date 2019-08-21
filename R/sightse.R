661868822792b5ba4179ba92231331ef6374ca04
Stuart Lee <lee.s@wehi.edu.au>
2019-08-21 10:06:51 +1000

    documentation for sightse class

--- R/sightse.R
+++ R/sightse.R
@@ -1,5 +1,6 @@
-#' Register tour_path as an S4 methods
-methods::setOldClass("tour_path")
+#' Register tour_path as an S4 class
+#' @importFrom methods setOldClass
+setOldClass("tour_path") 
 
 #' Represent a data.frame or matrix as a SightSE object.
 #' 
@@ -13,45 +14,81 @@ methods::setOldClass("tour_path")
 #' which is a callback function that implements a tour path, like
 #' `tourr::grand_tour()`.  
 #' 
+#' @param se a `SingleCellExperiment::SingleCellExperiment()` object
+#' @param traveller a function of class `tour_path` (usually tourr::grand_tour())
+#' @param .data object to convert to a `SightSE` object
+#' @param ... if `.data` is a data.frame the columns to convert to a matrix,
+#' every other column not included in `...` will become rowData
+#' @param view_as the type of matrix if `.data` is a data.frame (default is numeric matrix).
+#' 
 #' @importFrom SingleCellExperiment SingleCellExperiment
 #' @importFrom methods setClass setOldClass setGeneric
 #' @export
 #' 
-#' @name SightSE-class    
+#' @name SightSE
+#' @rdname SightSE-class     
 setClass("SightSE",
          slots = c("traveller" = "tour_path"),
          contains = "SingleCellExperiment")
 
-# constructor
+#' @name SightSE
+#' @rdname SightSE-class     
+#' @export
 SightSE <- function(se, traveller = tourr::grand_tour()) {
   new("SightSE", traveller = traveller, se)
 }
 
-# interface 
+#' @name SightSE
+#' @rdname SightSE-class 
+#' @export    
 setGeneric("as_sightse", function(.data, traveller = tourr::grand_tour(), ...) {
   standardGeneric("as_sightse")
 })
 
-setMethod("as_sightse", c("SingleCellExperiment", "tour_path"), function(.data, traveller) {
-  SightSE(.data, traveller)
-})
+#' @name SightSE
+#' @rdname SightSE-class 
+#' @export    
+setMethod("as_sightse", 
+          c("SingleCellExperiment", "tour_path"), 
+          function(.data, traveller) {
+            SightSE(.data, traveller)
+          })
 
-setMethod("as_sightse", c("SummarizedExperiment", "tour_path"), function(.data, traveller) {
-  SightSE(as(.data, "SingleCellExperiment"),  traveller)
-})
+#' @name SightSE
+#' @rdname SightSE-class 
+#' @export    
+setMethod("as_sightse", 
+          c("SummarizedExperiment", "tour_path"), 
+          function(.data, traveller) {
+            SightSE(as(.data, "SingleCellExperiment"),  traveller)
+          })
 
-setMethod("as_sightse", c("matrix", "tour_path"), function(.data, traveller) {
-  se <- SingleCellExperiment::SingleCellExperiment(list(view = .data))
-  SightSE(se, traveller)
-})
+#' @name SightSE
+#' @rdname SightSE-class 
+#' @export    
+setMethod("as_sightse", 
+          c("matrix", "tour_path"), 
+          function(.data, traveller) {
+            se <- SingleCellExperiment::SingleCellExperiment(list(view = .data))
+            SightSE(se, traveller)
+          })
 
-setMethod("as_sightse", c("data.frame", "tour_path"), function(.data, traveller, ...) {
-  # select the matrix part
-  mat_part <- dplyr::select(.data, ...)
-  row_part <- dplyr::select(.data, -rlang::enquos(...))
-  se <- SingleCellExperiment::SingleCellExperiment(
-    assays = list(view = mat_part),
-    rowData = row_part
-  )
-  SightSE(se, traveller)
-})
+#' @name SightSE
+#' @rdname SightSE-class 
+#' @export    
+setMethod("as_sightse", 
+          c("data.frame", "tour_path"), 
+          function(.data, traveller, ..., view_as = "matrix") {
+            # select the matrix part
+            if (!requireNamespace("dplyr", quietly = TRUE)) {
+              stop("Please install dplyr", call. = FALSE)
+            }
+            
+            mat_part <- as(dplyr::select(.data, ...), view_as)
+            row_part <- dplyr::select(.data, -rlang::enquos(...))
+            se <- SingleCellExperiment::SingleCellExperiment(
+              assays = list(view = mat_part),
+              rowData = row_part
+            )
+            SightSE(se, traveller)
+          })
