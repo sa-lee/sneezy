c4e52c8ce364bdec0b60bc9775a9ff50847a333f
Stuart Lee <lee.s@wehi.edu.au>
2019-09-02 09:36:16 +1000

    rename decompose

--- R/embed-linear.R
+++ R/embed-linear.R
@@ -1,9 +1,24 @@
+#' Linear Embedding Drivers
+#' 
+#' @details Different engines for computing 
+#' linear embeddings (mostly PCA at this stage). Usually you
+#' won't need to interact with these functions directly.
+#' 
+#' @return A `BiocSingular::BiocSingularParam()` object. 
+#' 
+#' @rdname le-backends
+#' @importFrom BiocSingular ExactParam IrlbaParam RandomParam
+#' @export
 pca_exact <- BiocSingular::ExactParam
+#' @rdname le-backends
+#' @export
 pca_irlba <- BiocSingular::IrlbaParam
+#' @rdname le-backends
+#' @export
 pca_random <- BiocSingular::RandomParam
 
 
-.arg_check_decompose <- function(.data, num_comp, center, scale, .parallel = BiocParallel::SerialParam()) {
+.arg_check_le <- function(.data, num_comp, center, scale, .parallel = BiocParallel::SerialParam()) {
   stopifnot(is(.data, "SightSE"))
   stopifnot(is.numeric(num_comp))
   stopifnot((is.logical(center) && length(center) == 1)|| length(center) == ncol(.data))
@@ -11,20 +26,21 @@ pca_random <- BiocSingular::RandomParam
   stopifnot(is(.parallel, "BiocParallelParam"))
 }
 
-#' Preprocess a SightSE via dimension reduction
+#' Compute a linear embedding on a `SightSE` object. 
 #' 
-#' @param .data A `SightSE`
+#' @param .data A `sneezy::SightSE()` object
 #' @param num_comp Number of components to retain
-#' @param center Should columns be centered?
+#' @param center Should columns be centered? Default = TRUE
 #' @param scale Should  columns be scaled to unit variance?
-#' @param .parallel A BiocParallel::BPPARAM object
-#' @param .method A BiocSingular object 
+#' @param .parallel A BiocParallel::BPPARAM() object
+#' @param .engine A BiocSingular::BiocSingular() object 
+#' used to compute the embedding. Deafults to `pca_exact()`  
 #' 
 #' @export 
-setGeneric("decompose", 
-           signature = c(".method"),
+setGeneric("embed_linear", 
+           signature = c(".engine"),
            function(.data, num_comp, center = TRUE, scale = FALSE, .parallel = BiocParallel::SerialParam(), .method) {
-             standardGeneric("decompose")
+             standardGeneric("embed_linear")
            })
 
 
@@ -33,9 +49,9 @@ setGeneric("decompose",
 #' @importFrom SummarizedExperiment assay
 #' @importFrom SingleCellExperiment reducedDim<- LinearEmbeddingMatrix
 #' @export
-setMethod("decompose", "BiocSingularParam", 
-          function(.data, num_comp, center = TRUE, scale = FALSE, .parallel = BiocParallel::SerialParam(), .method) {
-            .arg_check_decompose(.data, num_comp, center, scale, .parallel)
+setMethod("embed_linear", "BiocSingularParam", 
+          function(.data, num_comp, center = TRUE, scale = FALSE, .parallel = BiocParallel::SerialParam(), .engine = pca_exact()) {
+            .arg_check_le(.data, num_comp, center, scale, .parallel)
             res <- runPCA(assay(.data), 
                           rank = num_comp, 
                           center = center, 
@@ -51,9 +67,4 @@ setMethod("decompose", "BiocSingularParam",
               factorData = as(data.frame(sdev = res$sdev), "DataFrame")
             ) 
             .data
-          })
-
-
-
-
-
+})
