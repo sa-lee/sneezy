ef694fefa112cddaadfac55ce874f1ebf92fffa4
Stuart Lee <lee.s@wehi.edu.au>
2019-09-04 15:05:45 +1000

    update tests

--- tests/testthat/test-TourExperiment.R
+++ tests/testthat/test-TourExperiment.R
@@ -1,4 +1,4 @@
-context("SightSE class and constructor")
+context("TourExperiment class and constructor")
 
 suppressPackageStartupMessages(library(SingleCellExperiment))
 
@@ -6,39 +6,34 @@ olive <- tourr::olive
 rownames(olive) <- as.character(1:nrow(olive))
 
 test_that("constructor works", {
-  se_olive <- as_sightse(olive, 
-                         traveller = tourr::grand_tour(),
-                         palmitic:eicosenoic
-                         )
-  expect_s4_class(se_olive, "SightSE")
+  se_olive <- TourExperiment(olive,
+                             basisSets = SimpleList(),
+                             neighborSets = SimpleList(),
+                             palmitic:eicosenoic
+  )
+  expect_s4_class(se_olive, "TourExperiment")
   expect_true(is(se_olive, "SingleCellExperiment"))
-  expect_true(is(se_olive@traveller, "tour_path"))
   expect_identical(assayNames(se_olive), "view")
   
   # no selection puts everything into rowData
-  se_olive <- as_sightse(olive, 
-                         traveller = tourr::grand_tour(),
-  )
-  expect_s4_class(se_olive, "SightSE")
+  se_olive <- TourExperiment(olive)
+  expect_s4_class(se_olive, "TourExperiment")
   expect_true(is(se_olive, "SingleCellExperiment"))
-  expect_true(is(se_olive@traveller, "tour_path"))
   expect_equal(dim(assay(se_olive)), c(nrow(olive), 0))
   expect_identical(
     as.data.frame(rowData(se_olive)), olive 
   )
   
   # selecting throws an error if non-homegenous types
-  expect_error(as_sightse(olive, 
-                          traveller = tourr::grand_tour(),
+  expect_error(TourExperiment(olive, 
+                          basisSets = SimpleList(),
+                          neighborSets = SimpleList(),
                           1:ncol(olive)))
   
   # matrix constructor 
   sphere <- generate_sphere(1000, 10, 0, 1)
-  se_sphere <- as_sightse(sphere,
-                          traveller = tourr::grand_tour()
-                          )
-  expect_s4_class(se_sphere, "SightSE")
+  se_sphere <- TourExperiment(sphere)
+  expect_s4_class(se_sphere, "TourExperiment")
   expect_true(is(se_sphere, "SingleCellExperiment"))
-  expect_true(is(se_sphere@traveller, "tour_path"))
   expect_identical(assay(se_sphere), sphere)
 })
