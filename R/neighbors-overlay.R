a23fc15aa1279c20fc2e2e6b6d2aac45df866208
Stuart Lee <lee.s@wehi.edu.au>
2019-11-04 10:35:52 +1100

    clean up docs and make things a little more modular

--- R/neighbors-overlay.R
+++ R/neighbors-overlay.R
@@ -1,4 +1,4 @@
-#' Overlay a neighborSet on a set of xy coordinates
+#' Overlay a neighborSet onto a set of xy coordinates
 #' 
 #' @param x,y numeric vectors to produce segments from
 #' @param indices a neighborSet index matrix
@@ -6,20 +6,58 @@
 #' 
 #' @export
 #' @importFrom ggplot2 geom_segment
+#' @name overlay-neighbors
+#' @rdname overlay-neighbors
 overlay_neighbors <- function(x, y, indices, ...) {
-  
-  flattened <- flatten_graph(indices)
-  mesh <- create_mesh(x,y, flattened)
+  mesh <- .create_mesh(x,y, indices)
   add_segments(mesh, 
                ggplot2::vars(xend = xend, yend = yend),
                ...)
 }
 
-create_mesh <- function(x, y, flattened) {
-  data.frame(x = x[flattened[,1]],
-             y = y[flattened[,1]],
-             xend = x[flattened[,2]],
-             yend = y[flattened[,2]])
+#' @inheritParams overlay_neighbors
+#' @param type Algorithm used to compute shared nearest neighbors graph
+#' @export 
+#' @seealso [scran::neighborsToSNNGraph]
+#' @importFrom scran neighborsToSNNGraph neighborsToKNNGraph
+#' @importFrom igraph get.data.frame
+#' @export
+#' @name overlay-neighbors
+#' @rdname overlay-neighbors
+overlay_shared_neighbors <- function(x, y, indices, type = "rank", ...) {
+  mesh <- .create_mesh(x,y, indices, as = "snn", type =  type)
+  add_segments(mesh,
+               ggplot2::vars(xend = xend, yend = yend, alpha = weight),
+               ...)
+  
+}
+
+.create_mesh <- function(x, y, indices, as = "knn", ...) {
+  as <- match.arg(as, c("knn", "snn"))
+  if (as == "knn") {
+    edges <- igraph::get.data.frame(
+      scran::neighborsToKNNGraph(indices), 
+      what = "edges"
+    )
+    mesh <- .expand_by_edges(x, y, edges)
+  } else {
+    edges <- igraph::get.data.dframe(
+      scran::neighborsToSNNGraph(indices, ...),
+      what = "edges"
+    )
+    mesh <- .expand_by_edges(x, y, edges)
+    mesh[["weight"]] <- edges[["weight"]]
+  }
+  
+}
+
+.expand_by_edges <- function(x, y, edges) {
+  data.frame(
+    x = x[edges[,1]],
+    y = y[edges[,1]],
+    xend = x[edges[,2]],
+    yend = y[edges[,2]]
+  )
 }
 
 add_segments <- function(mesh, aes, ...) {
@@ -30,22 +68,8 @@ add_segments <- function(mesh, aes, ...) {
   )
 }
 
-#' Overlay a shared nearest neighbors graph onto an xy-scatter
-#' 
-#' @inheritParams overlay_neighbors
-#' @param type Algorithm used to compute shared nearest neighbors graph
-#' @export 
-#' @seealso [scran::neighborsToSNNGraph]
-#' @importFrom scran neighborsToSNNGraph neighborsToKNNGraph
-#' @importFrom igraph get.data.frame
-#' @export
-overlay_shared_neighbors <- function(x, y, indices, type = "rank", ...) {
-  flattened <- scran::neighborsToSNNGraph(indices, type = type)
-  flattened <- igraph::get.data.frame(flattened,what = "edges")
-  mesh <- cbind(create_mesh(x,y, flattened), 
-                weight = flattened[["weight"]])
-  add_segments(mesh,
-               ggplot2::vars(xend = xend, yend = yend, alpha = weight),
-               ...)
-  
+overlay_difference_neighbors <- function(x, y, set1, set2, ...) {
+  g1 <- scran::neighborsToKNNGraph(set1)
+  g2 <- scran::neighborsToKNNGraph(set2)
+  igraph::difference(g1, g2)
 }
